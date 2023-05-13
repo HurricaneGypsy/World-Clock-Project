b8b35051aff13ae323166d54912a63c61a72417a
PARDIP KAUR <95499357+HurricaneGypsy@users.noreply.github.com>
2023-05-13 19:29:27 +0100

    Added current location

--- index.js
+++ index.js
@@ -2,32 +2,48 @@ function updateTime() {
   // Los Angeles
   let losAngelesElement = document.querySelector("#los-angeles");
   if (losAngelesElement) {
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  losAngelesTime = moment().tz("America/Los_Angeles");
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    losAngelesTime = moment().tz("America/Los_Angeles");
 
-  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
   }
 
-  // Kenya
+  // Khartoum
   let khartoumElement = document.querySelector("#khartoum");
   if (khartoumElement) {
-  let khartoumDateElement = khartoumElement.querySelector(".date");
-  let khartoumTimeElement = khartoumElement.querySelector(".time");
-  khartoumTime = moment().tz("Africa/Khartoum");
+    let khartoumDateElement = khartoumElement.querySelector(".date");
+    let khartoumTimeElement = khartoumElement.querySelector(".time");
+    khartoumTime = moment().tz("Africa/Khartoum");
 
-  khartoumDateElement.innerHTML = khartoumTime.format("MMMM Do YYYY");
-  khartoumTimeElement.innerHTML = khartoumTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
-}
+    khartoumDateElement.innerHTML = khartoumTime.format("MMMM Do YYYY");
+    khartoumTimeElement.innerHTML = khartoumTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  // Dubai
+  let dubaiElement = document.querySelector("#dubai");
+  if (dubaiElement) {
+    let dubaiDateElement = dubaiElement.querySelector(".date");
+    let dubaiTimeElement = dubaiElement.querySelector(".time");
+    dubaiTime = moment().tz("Asia/Dubai");
+
+    dubaiDateElement.innerHTML = dubaiTime.format("MMMM Do YYYY");
+    dubaiTimeElement.innerHTML = dubaiTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 function updateCity (event) {
 let cityTimeZone = event.target.value;
+if (cityTimeZone === "current") {
+   cityTimeZone = moment.tz.guess();
+}
 let cityName = cityTimeZone.replace("_", " ").split("/")[1];
 let cityTime = moment().tz(cityTimeZone);
 
@@ -46,6 +62,7 @@ citiesElement.innerHTML = `
 
 updateTime();
 setInterval(updateTime, 1000);
+
 
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change",updateCity);
